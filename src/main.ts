dccca001a64967043df82aef4768270971b93d3b
Honza Kubita <honza.kubita@gmail.com>
2024-03-03 00:21:43 +0100

    feat: Inject iframe into webpage

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,7 @@
+import injectIframe from './modules/main/injectIframe';
+
 export default function runInMain() {
-    
+    // Inject the iframe into the website
+    // (also hides the website's content)
+    injectIframe();  
 }
---
+++ src/modules/main/injectIframe.ts
@@ -0,0 +1,21 @@
+export default function injectIframe() {
+    // Hide everything on the website
+    const nextDiv = document.getElementById("__next");
+
+    if (!nextDiv) return;
+
+    nextDiv.style.display = "none";
+    
+    // Create a new iframe with the same content that we can control
+    const iframe = document.createElement("iframe");
+    iframe.src = window.location.href;
+    iframe.style.width = "100vw";
+    iframe.style.height = "100vh";
+    iframe.style.position = "fixed";
+    iframe.style.top = "0";
+    iframe.style.left = "0";
+    iframe.style.border = "none";
+    iframe.style.zIndex = "10";
+
+    document.body.appendChild(iframe);
+}
